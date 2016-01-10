90944b92572ac702a8973f92f5c1694884cf5873
Toby Jones <aceroqs@users.noreply.github.com>
2016-01-10 08:07:28 -0800

    Remove fast output, as it was poorly designed.
    The specific dprintf is never directly called by client code.  Since the
    client may need to swap to the slow dprintf, depending on what is being
    logged, the design doesn't allow for easy swapping, except to reset the
    logger.  Setting the logger isn't thread safe, and the swapping is not
    ideal, so it is best to just remove the fast logger and always use the
    utf8-correct  logger.

--- DebuggerTracing.cpp
+++ DebuggerTracing.cpp
@@ -5,19 +5,11 @@
 namespace WindowsCommon
 {
 
-// This is a fast version of debugger_dprintf_utf8, which sacrifices
-// UTF-8 correctness for speed.  In most cases, this is fine, as the tracing
-// strings are internal and controlled, and speed is more important than
-// correctness for this type of debugging aid.
-void debugger_dprintf_fast(_In_z_ const char* output_string) noexcept
-{
-    OutputDebugStringA(output_string);
-}
-
 // Note that it is not possible to ensure that the debugging tool that is capturing
 // the traces has the fonts to display all required UTF-8 characters.
-void debugger_dprintf_utf8(_In_z_ const char* output_string) noexcept
+void debugger_dprintf(_In_z_ const char* output_string) noexcept
 {
+#ifndef NDEBUG
     const auto utf16_string = PortableRuntime::utf16_from_utf8(output_string);
     const auto c_string = utf16_string.c_str();
     const auto display_string = c_string[0] == L'!' ? c_string + 1 : c_string;
@@ -31,6 +23,9 @@ void debugger_dprintf_utf8(_In_z_ const char* output_string) noexcept
     }
 
     OutputDebugStringW(display_string);
+#else
+    (void)(output_string);
+#endif
 }
 
 }
--- DebuggerTracing.h
+++ DebuggerTracing.h
@@ -3,8 +3,7 @@
 namespace WindowsCommon
 {
 
-void debugger_dprintf_fast(_In_z_ const char* output_string) noexcept;
-void debugger_dprintf_utf8(_In_z_ const char* output_string) noexcept;
+void debugger_dprintf(_In_z_ const char* output_string) noexcept;
 
 }
 
